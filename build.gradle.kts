9bbcd4a690052893555dce9bfc1082a5a3f80f55
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-23 20:45:26 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.4" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
